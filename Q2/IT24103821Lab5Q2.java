04df3e1e68ef81dc4ac4752601d630e844a6ec04
IT24103821 <it24103821@my.sliit.lk>
2024-12-12 15:53:29 +0530

    Update IT24103821Lab5Q2.java

--- Q2/IT24103821Lab5Q2.java
+++ Q2/IT24103821Lab5Q2.java
@@ -4,15 +4,15 @@ public class IT24103821Lab5Q2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input the number of new members introduced
+   
         System.out.print("Enter the number of new members introduced: ");
         int newMembers = scanner.nextInt();
 
-        // Validate the entered number
+       
         if (newMembers < 0) {
             System.out.println("Invalid number. The number of new members should be greater than or equal to 0.");
         } else {
-            // Determine the prize using a switch statement
+          
             String prize;
             switch (newMembers) {
                 case 0:
@@ -35,7 +35,7 @@ public class IT24103821Lab5Q2 {
                     break;
             }
 
-            // Print the prize
+           
             System.out.println("The customer is entitled to: " + prize);
         }
 
